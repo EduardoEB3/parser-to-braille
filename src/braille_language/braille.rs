7c817b2be27d3f3b6ac3d3ae4262dced9307c7e7
EduardoEB3 <alu0101230382@ull.edu.es>
2023-11-25 21:28:42 +0000

    feat: Adding new characters and control for dots and acronyms

--- src/braille_language/braille.rs
+++ src/braille_language/braille.rs
@@ -32,13 +32,46 @@ impl Braille {
     pub fn encode(&self, text: String) -> String {
         let mut encoded_text = String::new();
 
-        for character in text.chars() {
-            if let Some(braille) = self.braille_map.get(&character) {
-                encoded_text.push_str(braille);
-            } else {
-                // Si el carácter no tiene un mapeo lo dejamos sin cambios
-                encoded_text.push(character);
+        // Dividir la cadena en palabras
+        let words: Vec<&str> = text.split_whitespace().collect();
+
+        // Obtener la cantidad total de palabras
+        let total_words = words.len();
+
+        // Iterar sobre las palabras y determinar si es la última
+        let mut word_index = 0;
+
+        let mut last_char = ' ';
+
+        while let Some(word) = words.get(word_index) {
+            let mut copy_word = word.to_string();
+            let mut char_index = 0;
+
+            if last_char.is_lowercase() && self.is_in_uppercase(word.to_string()) {
+                encoded_text.push('⠔');
+                last_char = ' ';
+                copy_word = copy_word.to_lowercase();
             }
+
+            while let Some(character) = copy_word.chars().nth(char_index) {
+                let next_char = word.chars().nth(char_index + 1).unwrap_or_default();
+
+                if character == '.' && next_char.is_uppercase() {
+                    // println!("Character: {} -> Next: {}", character, next_char);
+                    encoded_text.push_str(&self.find_character(next_char));
+                    char_index += 1;
+                } else {
+                    encoded_text.push_str(&self.find_character(character));
+                }
+                last_char = character;
+                char_index += 1;
+            }
+
+            if word_index != total_words - 1 {
+                encoded_text.push(' ');
+            }
+
+            word_index += 1;
         }
 
         encoded_text
@@ -48,5 +81,17 @@ impl Braille {
         for (letter, braille) in &self.braille_map {
             println!("{}: {}", letter, braille);
         }
+    }
+
+    fn find_character(&self, character: char) -> String {
+        if let Some(braille) = self.braille_map.get(&character) {
+            return braille.to_string();
+        }
+        // Si el carácter no tiene un mapeo lo dejamos sin cambios
+        return character.to_string();
+    }
+
+    fn is_in_uppercase(&self, word: String) -> bool {
+        word.chars().all(|char| char.is_uppercase())
     }
 }
